ee13945bd9473e76b0b40076836f76db916ce8b2
Bryan <bryankeefecresswell@gmail.com>
2017-03-15 23:59:31 +0800

    Fixed initwelcome.sh

--- initwelcome.sh
+++ initwelcome.sh
@@ -9,7 +9,7 @@ curl -X POST -H "Content-Type: application/json" -d '{
   "greeting":{
     "text":"Hello {{user_first_name}}, welcome to NewsBot."
   }
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
 
 # Get Started button
 curl -X POST -H "Content-Type: application/json" -d '{
@@ -20,4 +20,4 @@ curl -X POST -H "Content-Type: application/json" -d '{
       "payload":"Get Started"
     }
   ]
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
