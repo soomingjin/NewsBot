72539362602c86c6399893dc7eea2f8f95eb07f5
Bryan <bryankeefecresswell@gmail.com>
2017-03-15 23:56:51 +0800

    Added shell script

---
+++ initwelcome.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# initwelcome.sh: sets welcome screen
+# Execute once only
+
+# Greeting Text
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type":"greeting",
+  "greeting":{
+    "text":"Hello {{user_first_name}}, welcome to NewsBot."
+  }
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
+
+# Get Started button
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type":"call_to_actions",
+  "thread_state":"new_thread",
+  "call_to_actions":[
+    {
+      "payload":"Get Started"
+    }
+  ]
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$EAAEaTYHPhpEBAPZA8olwdCxhsZAx5ZCJgc88lnXTsEOIP5LEqJtsrhJTix9H9CXVNZCWSuN3GtzSvaTMosfIQmE4VZAjHFkKiAGjrYxyY3AoR14ZBMY2Gz6dLKV1KYBqMfVMGRZCzBqEUOflhkuBUGShg8AKdCiW4HyadTHcv0yswZDZD"
