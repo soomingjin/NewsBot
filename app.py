f864c5d9f3fb6ecc506a384d09a164e6de814dbb
Bryan <bryankeefecresswell@gmail.com>
2017-03-20 01:47:19 +0800

    Updated

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if (message_text.isdigit() and entry[0].messaging[0].message):
+                    if (message_text.isdigit() and messaging_event["message"]):
                         send_message(sender_id, "You have %s minutes to read? That's short!" % message_text)
                         send_generic_template(sender_id)
                         
