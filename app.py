b31528689cbe80c5ce978402d4daab2348a974f3
Bryan <bryankeefecresswell@gmail.com>
2017-03-23 20:21:23 +0800

    updated

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]
-                    if not booledSearch and not booledTime:
+                    if not booledSearch:
                         log("is this {bool}".format(bool = booledSearch))
                         searchQuery = message_text
                         booledSearch = True
