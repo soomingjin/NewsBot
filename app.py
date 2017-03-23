76917c85788ebf70a73e33cb1ef10e8e1dcf7bc5
Bryan <bryankeefecresswell@gmail.com>
2017-03-23 15:59:56 +0800

    update heroku

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ dictionary = dict()
 payloadFinal = ""
 # Dictionary to contain info about the news. title = {link : image}
 ultraDictOfNews = dict()
-searchQuery = ""
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -50,14 +49,11 @@ def webhook():
                     # TODO: Fix payload being detected and sent into query properly
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]
-                    if not message_text.isdigit():  # the message's text
-                        searchQuery += re.sub(r"\s", "+", message_text)
+                    message_text = messaging_event["message"]["text"]  # the message's text
                     if (message_text.isdigit() and int(message_text) <= 5):
                         send_message(sender_id, "You have %s minutes to read? That's short! Anyway, here you go!" % message_text)
                         log("passed1")
                         result = send_feed(searchQuery, int(message_text))
-                        log("passed3")
                         send_message(sender_id, result)
                         # To fix sending the generic template
                         # send_generic_template(sender_id)
@@ -67,7 +63,8 @@ def webhook():
                         send_message(sender_id, result)
                     else:
                         send_message(sender_id, "Sure, I'll find some %s articles for you!" % message_text)
-                        log("passed2")
+                        searchQuery = re.sub(r"\s", "+", message_text)
+                        log("passed")
                         send_message(sender_id, "Choose how much time you have to read! (in minutes)")
 
                 elif messaging_event.get("delivery"):  # delivery confirmation
