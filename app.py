ed2b4098a1acaf6d351c403d43bae5eee5bae019
Bryan <bryankeefecresswell@gmail.com>
2017-03-23 16:55:23 +0800

    included quick replies

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ payloadFinal = ""
 # Dictionary to contain info about the news. title = {link : image}
 ultraDictOfNews = dict()
 imageURL = ""
+timeToRead = null
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -50,31 +51,34 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]
-                    if (message_text.isdigit() == False):
-                        global searchQuery
-                        searchQuery = message_text  # the message's text
-                    if (message_text.isdigit() and int(message_text) <= 5):
+                    global searchQuery
+                    global timeToRead
+                    if (message_text.isdigit() == False and searchQuery != True):
+                        searchQuery = message_text
+                        send_message(sender_id, "Sure, I'll find some %s articles for you!" % message_text)
+                        send_message(sender_id, "Choose how much time you have to read! (in minutes)")  # the message's text
+                    elif (timeToRead == null and searchQuery):
+                        timeToRead = int(message_text)
+                    elif (timeToRead <= 5):
                         send_message(sender_id, "You have %s minutes to read? That's short! Anyway, here you go!" % message_text)
-                        log("passed1")
-                        result = send_feed(searchQuery, int(message_text))
+                        result = send_feed(searchQuery, timeToRead)
                         send_message(sender_id, result)
+                        send_quick_reply(sender_id)
                         # To fix sending the generic template
                         # send_generic_template(sender_id)
-                    elif (message_text.isdigit() and int(message_text) > 5):
+                    elif (timeToRead > 5):
                         send_message(sender_id, "Alright, get ready for a long read!")
                         result = send_feed(searchQuery, int(message_text))
                         send_message(sender_id, result)
+                        send_quick_reply(sender_id)
                     else:
-                        send_message(sender_id, "Sure, I'll find some %s articles for you!" % message_text)
-                        searchQuery = re.sub(r"\s", "+", message_text)
-                        log("passed")
-                        send_message(sender_id, "Choose how much time you have to read! (in minutes)")
+                        send_message(sender_id, "I don't really understand you... :(")
 
                 elif messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
-                elif messaging_event.get("optin"):  # optin confirmation
-                    pass
+                elif messaging_event.get("quick_reply"):  # user clicked "quick_reply"
+                    received_quick_reply(messaging_event)
 
                 elif messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     received_postback(messaging_event)
@@ -136,6 +140,30 @@ def received_postback(event):
     else:
         send_message(sender_id, "Postback recieved")
 # TODO: Fix up this method call to send carousels as well. (sending template)
+
+def received_quick_reply(event):
+    sender_id = event["sender"]["id"]
+    recipient_id = event["recipient"]["id"]
+    payload = event["quick_reply"]["payload"]
+    log("Received quick reply!")
+    if payload == "next":
+        randomKey = random.choice(ultraDictOfNews.keys())
+        linkToArticle = ultraDictOfNews[randomKey]['link']
+        stringResult = "This article is %.1f minute(s): %s (Link: %s)" % (ultraDictOfNews[randomKey]['time'], randomKey, linkToArticle)
+        del ultraDictOfNews[randomKey]
+        send_message(sender_id, stringResult)
+
+    elif payload == "search":
+        searchQuery = ""
+        send_message(sender_id, "Alright, let's search for something else!")
+        send_message(sender_id, "What would you like to search for?")
+
+    elif payload == "change":
+        timeToRead = null
+        send_message(sender_id, "You'd like to change your read time eh?")
+        send_message(sender_id, "Enter the new amount of time you'd like to spend reading!")
+
+
 def send_postback_button(recipient_id):
     log("sending postback message to {recipient}".format(recipient = recipient_id))
     data = json.dumps({
@@ -179,6 +207,45 @@ def send_postback_button(recipient_id):
         log(r.status_code)
         log(r.text)
 
+def send_quick_reply(recipient_id):
+    log("sending quick_reply to {recipient}".format(recipient = recipient_id))
+    data = json.dumps({
+        "recipient": {
+            "id":recipient_id
+        },
+        "message":{
+            "text":"Done reading? What next?",
+            "quick_replies":[
+                {
+                    "content_type":"text",
+                    "title":"Next Article",
+                    "payload":"next"
+                },
+                {
+                    "content_type":"text",
+                    "title":"Change Search Value",
+                    "payload":"search"
+                }
+                {
+                    "content_type":"text",
+                    "title":"Change Reading Time",
+                    "payload":"change"
+                }
+            ]
+        }
+    }'    
+    })
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
 def read_time(link):
     articleLink = Article(link)
     articleLink.download()
@@ -205,7 +272,7 @@ def send_feed(payload, timeToRead):
             ultraDictOfNews[post.title] = {'time':totalRead, 'image':imageURL, 'link':post.link}
     randomKey = random.choice(ultraDictOfNews.keys())
     linkToArticle = ultraDictOfNews[randomKey]['link']
-    stringResult = "This article is %.1f minutes: %s (Link: %s)" % (ultraDictOfNews[randomKey]['time'], randomKey, linkToArticle)
+    stringResult = "This article is %.1f minute(s): %s (Link: %s)" % (ultraDictOfNews[randomKey]['time'], randomKey, linkToArticle)
     del ultraDictOfNews[randomKey]
     return stringResult
 
